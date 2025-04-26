cb6ef94795fb21b4d1a2c121a4a76fc459c1facb
lucal <lucalazzeri97@gmail.com>
2025-04-26 19:47:37 +0200

    first commit

---
+++ src/main/java/com/example/Plugin.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ * demo java plugin
+ */
+public class Plugin extends JavaPlugin
+{
+  private static final Logger LOGGER=Logger.getLogger("demo");
+
+  public void onEnable()
+  {
+    LOGGER.info("demo enabled");
+  }
+
+  public void onDisable()
+  {
+    LOGGER.info("demo disabled");
+  }
+}
